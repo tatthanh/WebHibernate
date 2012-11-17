e0bdc0c42debc8dd2219306a0c5bac9604875504
tatthanh <a4registration@yahoo.com>
2012-11-17 17:59:58 +0700

    init

---
+++ NewPersonal/src/pojo/Person.java
@@ -0,0 +1,29 @@
+package pojo;
+
+public class Person {
+	private int Id;
+	private String Name;
+	private String Address;
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+}
---
+++ NewPersonal/src/util/HibernateUtil.java
@@ -0,0 +1,36 @@
+package util;
+
+import org.hibernate.SessionFactory;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtil {
+
+	private static final SessionFactory sessionFactory;
+
+	static {
+
+		try {
+			Configuration configuration = new Configuration().configure();
+			ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+		} catch (Throwable ex) {
+
+			System.err.println("Initial SessionFactory creation failed." + ex);
+
+			throw new ExceptionInInitializerError(ex);
+
+		}
+
+	}
+
+	public static SessionFactory getSessionFactory() {
+
+		return sessionFactory;
+
+	}
+}
+
